0347dfc865b55e26122c72632bba722d110ad30d
MayurValte <mayurvalte1999@gmail.com>
2021-05-30 19:35:01 +0530

    Some Work Remaing

---
+++ web.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	  <style>
+     *{
+      margin: 0; padding: 0;
+      /*font-family: 'Oswald', sans-serif;*/
+      font-family: 'Shadows Into Light', cursive;
+     } 
+    </style>
+</head>
+<body>
+	<h2>Download Link is here</h2>
+	<br>
+	<a href="pdf/dreamdesigner.pdf"> Click Here</a>
+
+</body>
+</html>
